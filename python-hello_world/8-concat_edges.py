de2098d3495c290229ae7a5074c997bf16b1b2f7
Amin Israfilov <israfilovamin@mail.ru>
2026-03-13 12:26:56 +0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = ( "Python is an interpreted, interactive",
-    "object-oriented programming",
-    "language that combines remarkable power with very clear syntax")
-print(str[1] + " with Python")
+str = ("Python is an interpreted, interactive",
+       "object-oriented programming",
+       "language that combines remarkable power with very clear syntax")
+print(str[1] + " " + str[2][-22:-18] + " " + str[0][:6])
