cdbebd7427f45861622b6dc7d9ec56f8d99fa2d6
Amin Israfilov <israfilovamin@mail.ru>
2026-03-17 10:15:45 +0400

    Refactor islower function to remove print statements
    Removed print statements for lower and upper case checks.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) > 96 and ord(c) < 123:
-        print("{} is lower".format(c))
         return True
     elif ord(c) > 64 and ord(c) < 91:
-        print("{} is upper".format(c))
         return False
