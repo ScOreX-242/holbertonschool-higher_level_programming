07c3dd708676640081ff663cbb71fd5a33052559
Amin Israfilov <israfilovamin@mail.ru>
2026-03-19 07:32:40 +0000

    Syntax correction

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,4 +3,7 @@
 def replace_in_list(my_list, idx, element):
 	if idx < 0 or idx >= len(my_list):
 		return my_list
-	my_list[idx] = element
+	new_list = my_list
+	new_list[idx] = my_element
+	print(new_list)
+	print(my_list)
