6ee175381b6ba7659b7248595145e7c76e90aeb2
Amin Israfilov <israfilovamin@mail.ru>
2026-03-19 07:10:57 +0000

    Syntax correction

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 
 def element_at(my_list, idx):
 	if idx < 0:
-		return None
+		print("Element at index {:d} is None".format(idx)
 	elif idx > len(my_list):
-		return None
+		print("Element at index {:d} is None".format(idx)
 	else:
 		print("Element at index {:d} is {}".format(idx, my_list[idx]))
