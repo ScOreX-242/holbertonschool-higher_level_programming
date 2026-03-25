3ce5e78b2398ec1a7c1f83a5ad3a2b5c3f24dc88
Amin Israfilov <israfilovamin@mail.ru>
2026-03-25 10:46:28 +0000

    Syntax correction

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = []
+    new_list = my_list[:]
     for i in range (len(my_list)):
         if my_list[i] == search:
             my_list[i] = replace
-        new_list.append(my_list[i])
     return new_list
 
 
