a6e35463901fbed3ff87d7be5cfe4f85b2bdc2da
Amin Israfilov <israfilovamin@mail.ru>
2026-03-25 11:30:39 +0000

    Syntax correction

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-for item1 in set_1:
-    for item2 in set_2:
-        if item1 == item2:
-            print('[\'{}\']'.format(item1))
+    for item1 in set_1:
+        for item2 in set_2:
+            if item1 == item2:
+                print('[\'{}\']'.format(item1))
