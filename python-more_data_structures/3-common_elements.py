dbc2f33b883f6fdf282d22438839521dd339804a
Amin Israfilov <israfilovamin@mail.ru>
2026-03-25 15:36:10 +0400

    Update 3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
+    result = []
     for item1 in set_1:
         for item2 in set_2:
             if item1 == item2:
-                print('[\'{}\']'.format(item1))
-	        else:
-    		    return None
+                result.append(item1)
+    return result
