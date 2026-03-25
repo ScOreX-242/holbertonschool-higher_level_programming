299bf63fad75661c0ad7fa7dad281f80c8e2b76c
Amin Israfilov <israfilovamin@mail.ru>
2026-03-25 19:04:34 +0400

    Comment out empty dictionary check and max return
    Commented out the check for an empty dictionary and the return statement for finding the maximum key.

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -10,3 +10,8 @@ def best_score(a_dictionary):
             max_score = v
             best_key = k
     return best_key
+
+    
+# if not a_dictionary:
+#     return None
+# return max(a_dictionary, key=a_dictionary.get)
