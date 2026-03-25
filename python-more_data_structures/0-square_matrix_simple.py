38ada86cb30d152adaf0a65a34cf64c15c942cf9
Amin Israfilov <israfilovamin@mail.ru>
2026-03-25 10:07:17 +0000

    Syntax correction

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_row = []
-        for item in row[]:
+        for item in row:
             new_row.append(item ** 2)
     return new_matrix
