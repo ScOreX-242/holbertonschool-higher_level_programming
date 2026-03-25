b588667bb50577cb7c20fa4a3325a4b369b664e0
Amin Israfilov <israfilovamin@mail.ru>
2026-03-25 17:04:25 +0400

    Update 5-number_keys.py

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,3 +2,16 @@
 
 def number_keys(a_dictionary):
     return len(a_dictionary)
+
+# def number_keys(a_dictionary):
+#     count = 0
+#     for key in a_dictionary:
+#         count += 1           
+#     return count
+
+# def number_keys(a_dictionary):
+#     return len(a_dictionary.keys())
+
+# def number_keys(a_dictionary):
+#     keys_list = list(a_dictionary)
+#     return len(keys_list)
