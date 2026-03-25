c5765d859f06afd3530b0b50b7c532ddff710f77
Amin Israfilov <israfilovamin@mail.ru>
2026-03-25 15:02:16 +0000

    11 task

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_key = list(a_dictionary.keys())[0]
+    max_score = a_dictionary[best_key]
+    for k, v in a_dictionary.items():
+        if v > max_score:
+            max_score = v
+            best_key = k
+    return best_key
