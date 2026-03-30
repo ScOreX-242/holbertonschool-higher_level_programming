cb55736b9aa585f37da1dae76abd2aa11e6ca0fb
Amin Israfilov <israfilovamin@mail.ru>
2026-03-30 10:24:49 +0000

    1 task

---
+++ python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+ """
+    A class that defines a square.
+
+    This class represents a square shape. For now, it is an empty class
+    and does not contain any attributes or methods.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+README file for Classes and Objects tasks
