cc06c7a589d5d62e3d50693f06b65a0d22fccee7
Amin Israfilov <israfilovamin@mail.ru>
2026-03-30 10:27:40 +0000

    Syntax correction

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
 class Square:
- """
-    A class that defines a square.
-
-    This class represents a square shape. For now, it is an empty class
-    and does not contain any attributes or methods.
-
-    Attributes:
-        None
-
-    Methods:
-        None
-    """
+    """Represent a square."""
     pass
