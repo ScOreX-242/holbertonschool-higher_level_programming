2287ee508b4970d0b64c73be553ec968a7a2532b
Amin Israfilov <israfilovamin@mail.ru>
2026-04-16 23:39:00 +0400

    Create 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a student class."""
+
+
+class Student:
+    """defines a student"""
+  
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(item) is str for item in attrs):
+            filtered_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    filtered_dict[key] = self.__dict__[key]
+            return filtered_dict
+        
+        return self.__dict__
