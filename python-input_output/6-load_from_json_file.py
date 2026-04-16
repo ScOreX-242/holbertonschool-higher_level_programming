14c2ba2a4e683cca1382b012eacd036f3ea8c2af
Amin Israfilov <israfilovamin@mail.ru>
2026-04-16 08:06:14 +0000

    Syntax correction

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     '''Creates an Object from a "JSON file".'''
     with open(filename, "w", encoding="utf-8") as f:
-        return json.loads(filename)
+        return json.loads(f)
